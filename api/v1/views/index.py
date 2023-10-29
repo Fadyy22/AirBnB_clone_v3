f2c43dc9534dedbe70b26ca0dda71c320946fa64
Fadyy22 <fadyalaa441@gmail.com>
2023-10-30 00:08:42 +0200

    add route /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """script that contains route /status"""
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    return jsonify(
+        amenities=storage.count("Amenity"),
+        cities=storage.count("City"),
+        places=storage.count("Place"),
+        reviews=storage.count("Review"),
+        states=storage.count("State"),
+        users=storage.count("User"),
+    )
