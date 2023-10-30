18dbf1143d9ae64716fedae32d296bead83509a0
Reem-Kamal-Ghoniem <reemkamal11@gmail.com>
2023-10-30 16:06:29 -0700

    state object

---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+"""a new view for State objects that handles all default RESTFul API actions"""
+from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
+from models import storage
+from models.state import State
+
+
+@app.route('/api/v1/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all State objects: GET /api/v1/states"""
+    states = State.all()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def r_state_id(state_id):
+    """ GET a State object """
+    state = State.get(state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_state(state_id):
+    """ Deleting"""
+    state = State.get(state_id)
+    if not state:
+        abort(404)
+    state.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """ Creating a State object """
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    state = State(**data)
+    storage.new(state)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """ Updating a State object using PUT"""
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+
+    data.pop('id', None)
+    data.pop('created_at', None)
+    data.pop('updated_at', None)
+    for key, value in data.items():
+        setattr(state, key, value)
+
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
