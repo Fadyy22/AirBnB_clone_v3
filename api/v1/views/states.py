ff1a66eb604e93899be5e28a1c0ebba1d22d2ff1
Fadyy22 <fadyalaa441@gmail.com>
2023-10-31 17:45:53 +0200

    fix checker fails

---
+++ api/v1/views/states.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""a new view for State objects that handles all default RESTFul API actions"""
+from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
+from models import storage
+from models.state import State
+
+
+@app_views.get("/states", strict_slashes=False)
+def get_states():
+    """get a list of all states"""
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.get("/states/<state_id>", strict_slashes=False)
+def r_state_id(state_id):
+    """get state"""
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.delete("/states/<state_id>", strict_slashes=False)
+def del_state(state_id):
+    """delete state"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    state.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.post("/states", strict_slashes=False)
+def post_state():
+    """create a new state"""
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    state = State(**data)
+    storage.new(state)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.put("/states/<state_id>", strict_slashes=False)
+def put_state(state_id):
+    """update existing state"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+
+    data.pop("id", None)
+    data.pop("created_at", None)
+    data.pop("updated_at", None)
+    for key, value in data.items():
+        setattr(state, key, value)
+
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
