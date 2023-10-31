b2c4360871c431f7b37f1ac352d7cbde295cf625
Reem-Kamal-Ghoniem <reemkamal11@gmail.com>
2023-10-30 18:08:48 -0700

    states.edit 3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,27 +6,28 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects: GET /api/v1/states"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def r_state_id(state_id):
-    """ GET a State object """
-    state = storage.get("State", state_id)
+    """GET a State object"""
+    state = storage.get(State, state_id)
     if state:
         return jsonify(state.to_dict())
     else:
         abort(404)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route("/states/<state_id>", methods=["DELETE"],
                  strict_slashes=False)
 def del_state(state_id):
-    """ Deleting"""
-    state = storage.get("State", state_id)
+    """Deleting"""
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
     state.delete()
@@ -34,9 +35,9 @@ def del_state(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def post_state():
-    """ Creating a State object """
+    """Creating a State object"""
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
@@ -48,10 +49,10 @@ def post_state():
     return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def put_state(state_id):
-    """ Updating a State object using PUT"""
-    state = storage.get("State", state_id)
+    """Updating a State object using PUT"""
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
 
@@ -59,14 +60,11 @@ def put_state(state_id):
     if not data:
         abort(400, "Not a JSON")
 
-    data.pop('id', None)
-    data.pop('created_at', None)
-    data.pop('updated_at', None)
+    data.pop("id", None)
+    data.pop("created_at", None)
+    data.pop("updated_at", None)
     for key, value in data.items():
         setattr(state, key, value)
 
     storage.save()
     return make_response(jsonify(state.to_dict()), 200)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
